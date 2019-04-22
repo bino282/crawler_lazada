9198a14e8a94c68585d30547d4948d5e26ef8b47
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-04-22 10:10:32 +0700

    upfile

--- upload.py
+++ upload.py
@@ -11,5 +11,14 @@ def upload_to_bucket(blob_name, path_to_file, bucket_name):
     return blob.public_url
 
 list_file = os.listdir('./images')
+tmp = []
+with open('tmp.txt','r') as lines:
+    for line in lines:
+        tmp.append(line.strip())
+fw = open('tmp.txt','w')
 for file_name in list_file:
-    print(upload_to_bucket('all_imgs_products/{}'.format(file_name),'./images/{}'.format(file_name),'storage_ai2019'))
+    if file_name not in tmp:
+        print(upload_to_bucket('all_imgs_products/{}'.format(file_name),'./images/{}'.format(file_name),'storage_ai2019'))
+        fw.write(file_name)
+        fw.write('\n')
+fw.close()
